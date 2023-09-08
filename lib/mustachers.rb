cd4a025c0fae91eefc8336d2b7d0cc567aff3f8d
Sam French <sfrench_01@yahoo.co.uk>
2023-09-08 14:49:39 +0100

    Fix require and bump version

--- lib/mustachers.rb
+++ lib/mustachers.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative "mustachers/version"
-require_relative "mustachers/mustachers"
+require_relative 'mustachers/version'
+require 'mustachers/mustachers'
 
 module Mustachers
   class Error < StandardError; end
